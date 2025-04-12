5e8d568797bc38f265202da91ed4ebcd229f2931
jhonbellaizac <janerb123@gmail.com>
2025-04-11 23:03:32 -0500

    creando atributos a la clase mago

--- Parcial2/src/parcial2/Mago.java
+++ Parcial2/src/parcial2/Mago.java
@@ -1,13 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package parcial2;
+public class Mago extends Criatura implements Magico{
+    private String hechizo;
 
-/**
- *
- * @author Jhon Bellaizac
- */
-public class Mago {
+    public Mago(String hechizo, String nombre, int salud, int fuerza) {
+        super(nombre, salud, fuerza);
+        this.hechizo = hechizo;
+    }
     
+    @Override
+    public void atacar (Criatura objetivo){
+        objetivo.defender(fuerza);
+        }
+    @Override
+    public void defender (int daño){
+        salud -= daño;
+    }
+    
+    @Override
+    public void aprenderHechizo (String hechizo){
+        this.hechizo = hechizo;
+    }
+    @Override
+    public void lanzarHechizo(){
+        System.out.println(nombre + "lanzar hechizo: " + hechizo);
+    }
 }
